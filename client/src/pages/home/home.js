bf58f947df782067804fc2668c1bf94ad52ad770
hatem-ashraf <hatem195301@feng.bu.edu.eg>
2024-05-02 19:52:41 +0300

    Added Guest mode

--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -6,13 +6,13 @@ import { useNavigate } from 'react-router-dom';
 function HomePage() {
   const navigate = useNavigate(); // Hook to perform navigation
 
-  useEffect(() => {
-    const user = localStorage.getItem('User');
-    const token = localStorage.getItem('Token');
-    if (!user || !token) {
-      navigate('/'); // Redirect to login if either user or token is missing
-    }
-  }, [navigate]);
+  // useEffect(() => {
+  //   const user = localStorage.getItem('User');
+  //   const token = localStorage.getItem('Token');
+  //   if (!user || !token) {
+  //     navigate('/'); // Redirect to login if either user or token is missing
+  //   }
+  // }, []);
 
   return (
     <div>
